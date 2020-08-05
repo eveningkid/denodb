2fe220e333a10b6f67647d8865b3b0a3656d36f3
fauzan121002 <developerfauzan@gmail.com>
2020-08-05 23:42:18 +0700

    https://deno.land/x/color/index.ts is deprecated.

--- lib/helpers/log.ts
+++ lib/helpers/log.ts
@@ -1,13 +1,13 @@
 import { ConsoleColor } from "../../deps.ts";
 
 export function success(message: string) {
-  console.log(ConsoleColor.green.text(message));
+  console.log(ConsoleColor.success(message));
 }
 
 export function error(message: string) {
-  console.log(ConsoleColor.red.text(message));
+  console.log(ConsoleColor.error(message));
 }
 
 export function warning(message: string) {
-  console.log(ConsoleColor.yellow.text(message));
+  console.log(ConsoleColor.warning(message));
 }
