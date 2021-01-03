b565170020b8961815d6a7b37ccaee3b574dcd0f
Edward Bebbington <edward.bebbington@intercity.technology>
2021-01-03 13:56:09 +0000

    [chore/version-deps] Fix typo

--- deps.ts
+++ deps.ts
@@ -9,7 +9,7 @@ export {
   Connection as MySQLConnection,
 } from "https://deno.land/x/mysql@v2.7.0/mod.ts";
 
-export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.6.8/mod.ts";
+export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.4.6/mod.ts";
 
 export { DB as SQLiteClient } from "https://deno.land/x/sqlite@v2.3.1/mod.ts";
 
