f667316e63e2d3b62c66fe7feeb0c5b80e860a46
EightDoor <zhou851708184@163.com>
2021-03-18 21:53:31 +0800

    fix: dev not fond 404

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export * as ConsoleColor from "https://deno.land/x/colorlog@v1.0/mod.ts";
 
-export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/denjucks/dex/master/mod.ts";
+export { default as SQLQueryBuilder } from "https://deno.land/x/dex@1.0.2/mod.ts";
 
 export { camelCase, snakeCase } from "https://deno.land/x/case/mod.ts";
 
