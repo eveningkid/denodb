7bf060c14f4b0ac33a13de219e7b25afb5531d5f
Arnaud <eveningkid@users.noreply.github.com>
2021-03-30 19:00:57 +0100

    fix: update broken Dex dependency

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export * as ConsoleColor from "https://deno.land/x/colorlog@v1.0/mod.ts";
 
-export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/denjucks/dex/master/mod.ts";
+export { default as SQLQueryBuilder } from "https://raw.githubusercontent.com/aghussb/dex/master/mod.ts";
 
 export { camelCase, snakeCase } from "https://deno.land/x/case/mod.ts";
 
