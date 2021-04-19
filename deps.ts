1706cd5f33e2b141f68806b8e76e3e054824cb01
eveningkid <arn.dellinger@gmail.com>
2021-04-19 23:23:07 +0100

    chore: upgrade to postgres@0.11.1

--- deps.ts
+++ deps.ts
@@ -11,7 +11,7 @@ export {
   Connection as MySQLConnection,
 } from "https://deno.land/x/mysql@v2.8.0/mod.ts";
 
-export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.4.6/mod.ts";
+export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.11.1/mod.ts";
 
 export { DB as SQLiteClient } from "https://deno.land/x/sqlite@v2.3.1/mod.ts";
 
