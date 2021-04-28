9a19bdb22ec2791aa7921f23c6206759ac2e2d0d
Arnaud <eveningkid@users.noreply.github.com>
2021-04-28 21:43:13 +0100

    chore: upgrade postgres driver to 0.11.2

--- deps.ts
+++ deps.ts
@@ -13,7 +13,7 @@ export {
 } from "https://deno.land/x/mysql@v2.8.0/mod.ts";
 export type { LoggerConfig } from "https://deno.land/x/mysql@v2.8.0/mod.ts";
 
-export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.11.1/mod.ts";
+export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.11.2/mod.ts";
 
 export { DB as SQLiteClient } from "https://deno.land/x/sqlite@v2.3.1/mod.ts";
 
