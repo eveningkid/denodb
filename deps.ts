5d05519d7ebe221c4bcc78aeba1e4e4840e3bd72
Jared Peña Ochoa <jaredpenaochoa@outlook.com>
2021-08-11 17:53:15 -0500

    Fix (#289): MySQL & Mongo's drivers upgraded

--- deps.ts
+++ deps.ts
@@ -10,13 +10,13 @@ export {
   Client as MySQLClient,
   configLogger as configMySQLLogger,
   Connection as MySQLConnection,
-} from "https://deno.land/x/mysql@v2.8.0/mod.ts";
-export type { LoggerConfig } from "https://deno.land/x/mysql@v2.8.0/mod.ts";
+} from "https://deno.land/x/mysql@v2.10.0/mod.ts";
+export type { LoggerConfig } from "https://deno.land/x/mysql@v2.10.0/mod.ts";
 
 export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.11.2/mod.ts";
 
 export { DB as SQLiteClient } from "https://deno.land/x/sqlite@v2.3.1/mod.ts";
 
-export { MongoClient as MongoDBClient, Bson } from "https://deno.land/x/mongo@v0.22.0/mod.ts";
-export type { ConnectOptions as MongoDBClientOptions } from "https://deno.land/x/mongo@v0.22.0/mod.ts";
-export type { Database as MongoDBDatabase } from "https://deno.land/x/mongo@v0.22.0/src/database.ts";
+export { MongoClient as MongoDBClient, Bson } from "https://deno.land/x/mongo@v0.24.0/mod.ts";
+export type { ConnectOptions as MongoDBClientOptions } from "https://deno.land/x/mongo@v0.24.0/mod.ts";
+export type { Database as MongoDBDatabase } from "https://deno.land/x/mongo@v0.24.0/src/database.ts";
